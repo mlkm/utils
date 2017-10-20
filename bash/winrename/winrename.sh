6108ba99a2fa9e7ec8d6724aa638853fe813e328
mlkm <miloszzz+mlkm@gmail.com>
2017-10-20 11:13:54 +0200

    Make winrename preserve real window names

--- bash/winrename/winrename.sh
+++ bash/winrename/winrename.sh
@@ -4,32 +4,33 @@ cd "$(/usr/bin/dirname $(/bin/readlink -e "${0}"))"
 source vars.sh
 
 function run_bg {
-  local NAME=${1}
+  local NAME="${1}"
   shift
 
-  local TARGET=$(/bin/readlink -e "${1}")
+  local TARGET="$(/bin/readlink -e "${1}")"
   shift
 
-  local MKTEMP_DIR=$(/bin/mktemp -d)
+  local MKTEMP_DIR="$(/bin/mktemp -d)"
 
   /bin/ln -sT "${TARGET}" "${MKTEMP_DIR}/${NAME}"
 
   /usr/bin/nohup "${MKTEMP_DIR}/${NAME}" "${@}" &>> "${TMP_DIR}/${NAME}.log" &
 }
 
-# Asks the user to click a window.
-WID=$(/usr/bin/xdotool selectwindow)
+WIN_NAME_PREFIX="${*}"
 
-NAME="wnrnm${WID}"
-WIN_NAME=${*}
-
-/usr/bin/killall -- "${NAME}" &> /dev/null | true
-if [[ -n "${WIN_NAME}" ]]; then
-  run_bg "${NAME}" worker.sh "${WID}" "${WIN_NAME}"
-else
+if [[ -z "${WIN_NAME_PREFIX}" ]]; then
   /bin/echo >&2 "
 Usage:
-  ${0} WIN_NAME
+  ${0} WIN_NAME_PREFIX
 "
   exit 1
 fi
+
+# Asks the user to click a window.
+WID="$(/usr/bin/xdotool selectwindow)"
+
+NAME="wnrnm${WID}"
+
+/usr/bin/killall -- "${NAME}" &> /dev/null | true
+run_bg "${NAME}" worker.sh "${WID}" "${WIN_NAME_PREFIX}"
--- bash/winrename/worker.sh
+++ bash/winrename/worker.sh
@@ -2,12 +2,22 @@
 set -e
 source vars.sh
 
-WID=${1}
+WID="${1}"
 shift
 
-WIN_NAME=${*}
+WIN_NAME_PREFIX="[${*}] "
 
 while true; do
+  WIN_NAME="$(/usr/bin/xdotool getwindowname "${WID}")"
+
+  # Strip all occurences of WIN_NAME_PREFIX from the beginning of WIN_NAME.
+  while [[ "${WIN_NAME}" == "${WIN_NAME_PREFIX}"* ]]; do
+    WIN_NAME="${WIN_NAME:${#WIN_NAME_PREFIX}}"
+  done
+
+  # Prepend WIN_NAME_PREFIX to WIN_NAME.
+  WIN_NAME="${WIN_NAME_PREFIX}${WIN_NAME}"
+
   /usr/bin/xdotool set_window --name "${WIN_NAME}" "${WID}"
 
   /bin/sleep "${TICKER_PERIOD}"
